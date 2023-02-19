8471abe348472c4eee1d12f0c9571e6c4337b767
Lewis ushindi <coderflame3@gmail.com>
2023-02-19 16:18:44 +0300

    firebase auth

--- public/app.js
+++ public/app.js
@@ -1,10 +1,9 @@
-// sidenav
 /* Open the sidenav */
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("Serita-db").style.marginLeft="200px";
   }
-  
+    
   /* Close the sidenav */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
@@ -12,29 +11,34 @@ function openNav() {
   }
 
 //   login page
-var firebaseConfig = {
-    // your Firebase configuration settings
-};
-firebase.initializeApp(firebaseConfig);
-
-// set up event listener on the login form
-$(document).ready(function() {
-    $("#loginForm").submit(function(event) {
-        event.preventDefault();
-        var email = $("input[name='email']").val();
-        var password = $("input[name='password']").val();
-        firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // redirect to dashboard on successful login
-            window.location.href = "dashboard.html";
-        })
-        .catch((error) => {
-            // handle login error
-            Swal.fire({
-                icon: 'error',
-                title: 'Login failed',
-                text: error.message
+const firebaseConfig = {
+    apiKey: "AIzaSyATbNjJXMWC-F-LPD8H6IukvN2KVmD3gIs",
+    authDomain: "seritahealth.firebaseapp.com",
+    projectId: "seritahealth",
+    storageBucket: "seritahealth.appspot.com",
+    messagingSenderId: "597467249811",
+    appId: "1:597467249811:web:e6ddf3d183ce0683352166",
+    measurementId: "G-G2GKC4MZ14"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+    
+    $(document).ready(function() {
+        $("#loginForm").submit(function(event) {
+            event.preventDefault();
+            var email = $("input[name='email']").val();
+            var password = $("input[name='password']").val();
+            auth.signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {					
+                window.location.href = "dashboard.html";
             })
+            .catch((error) => {					
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login failed',
+                    text: error.message
+                })
+            });
         });
     });
-});

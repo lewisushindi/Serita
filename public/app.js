e8c481b25fec71ef759c2d45b69bea92bba432aa
Lewis ushindi <coderflame3@gmail.com>
2023-02-19 16:14:33 +0300

    fetching user data from firebase database

--- public/app.js
+++ public/app.js
@@ -10,4 +10,31 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("Serita-db").style.marginLeft="4px";
   }
-  
+
+//   login page
+var firebaseConfig = {
+    // your Firebase configuration settings
+};
+firebase.initializeApp(firebaseConfig);
+
+// set up event listener on the login form
+$(document).ready(function() {
+    $("#loginForm").submit(function(event) {
+        event.preventDefault();
+        var email = $("input[name='email']").val();
+        var password = $("input[name='password']").val();
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // redirect to dashboard on successful login
+            window.location.href = "dashboard.html";
+        })
+        .catch((error) => {
+            // handle login error
+            Swal.fire({
+                icon: 'error',
+                title: 'Login failed',
+                text: error.message
+            })
+        });
+    });
+});

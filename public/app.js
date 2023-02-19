2cd7442f908602fdf957481b9e4b1eee4439d101
Lewis ushindi <coderflame3@gmail.com>
2023-02-19 15:42:52 +0300

    creating dashboard

---
+++ public/app.js
@@ -0,0 +1,13 @@
+// sidenav
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("Serita-db").style.marginLeft="200px";
+  }
+  
+  /* Close the sidenav */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("Serita-db").style.marginLeft="4px";
+  }
+  
